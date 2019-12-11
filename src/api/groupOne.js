647068c80666bbc20dbb5a48412f8ff6c28b7fa1
Rick <Rick.Carosa@ad.eagledream.com>
2019-12-11 12:40:13 -0500

    remove .then from api call

--- src/api/groupOne.js
+++ src/api/groupOne.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 const URL = 'https://swapi.co/api/people/?page=1';
 
 export const fetchCharacterGroupOne = () => {
-  return axios.get(URL).then(res => res);
+  return axios.get(URL);
 };
--- src/api/groupTwo.js
+++ src/api/groupTwo.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 const URL = 'https://swapi.co/api/people/?page=2';
 
 export const fetchCharacterGroupTwo = () => {
-  return axios.get(URL).then(res => res);
+  return axios.get(URL);
 };
